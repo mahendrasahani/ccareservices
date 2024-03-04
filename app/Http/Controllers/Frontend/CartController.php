e2bfff4c6577300d157add6df0d2da550fd8574e
Developer_Anil_Kumar <kum09a@gmail.com>
2024-03-04 17:33:03 +0530

    update

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Session;
 class CartController extends Controller
 {
     public function showCart(){ 
-        return view('frontend.cart', compact('cart'));
+        return view('frontend.cart');
     }
 
     public function addToCart(Request $request){
